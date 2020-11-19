31bc645abcfd9b818f8d67324e6cdbf63a0f0b02
Raigorodskyi <raigorodskyi@gmail.com>
2020-11-19 12:19:27 -0500

    Added the method that will stop the game if the snake is touching the car and also added the role to the keys up down left and right

--- SnakyRoad/Snake.java
+++ SnakyRoad/Snake.java
@@ -16,20 +16,26 @@ public class Snake extends Actor
     public void act() 
     {
         checkKeyPress();
+        isTouchingTheCar();
     }
     
     public void checkKeyPress(){
-        if (Greenfoot.isKeyDown("d")){
+        if (Greenfoot.isKeyDown("d") || Greenfoot.isKeyDown("right")){
             setLocation(getX()+3, getY());
         }
-        if (Greenfoot.isKeyDown("a")){
+        if (Greenfoot.isKeyDown("a") || Greenfoot.isKeyDown("left")) {
             setLocation(getX()-3, getY());
         }
-        if (Greenfoot.isKeyDown("w")){
+        if (Greenfoot.isKeyDown("w") || Greenfoot.isKeyDown("up")){
             setLocation(getX(), getY()-3);
         }
-        if (Greenfoot.isKeyDown("s")){
+        if (Greenfoot.isKeyDown("s") || Greenfoot.isKeyDown("down")){
             setLocation(getX(), getY()+3);
+        }
+    }
+    public void isTouchingTheCar(){
+        if (isTouching(Car.class)){
+            Greenfoot.stop();
         }
     }
 }
