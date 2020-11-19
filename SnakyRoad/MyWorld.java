0551fe660afe196d8eb38643ff741ee2a21c9feb
Raigorodskyi <raigorodskyi@gmail.com>
2020-11-19 15:34:07 -0500

    Made the cars appear randomly from both sides. Also created a new class carToLeft that will make a few rows of the cars go from right to left

--- SnakyRoad/MyWorld.java
+++ SnakyRoad/MyWorld.java
@@ -8,6 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
+    //private int count = 0;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -28,12 +29,35 @@ public class MyWorld extends World
     {
         Snake snake = new Snake();
         addObject(snake,276,364);
-        Car car = new Car();
+        /*Car car = new Car();
         addObject(car,30,49);
         Car car2 = new Car();
         addObject(car2,21,151);
         Car car3 = new Car();
         addObject(car3,18,239);
-        removeObject(car3);
+        removeObject(car3);*/
     }
+    public void act()
+    {
+        if (Greenfoot.getRandomNumber(200)<1)
+        {
+            addObject(new Car(), 1, 50);
+        }
+        if (Greenfoot.getRandomNumber(200)<1)
+        {
+            addObject(new Car(), 1, 90);
+        }
+        if (Greenfoot.getRandomNumber(200)<1)
+        {
+            addObject(new Car(), 1, 130);
+        }
+        if (Greenfoot.getRandomNumber(200)<1)
+        {
+            addObject(new CarToLeft(), 599, 170);
+        }
+        if (Greenfoot.getRandomNumber(200)<1)
+        {
+            addObject(new CarToLeft(), 599, 210);
+        }
+    } 
 }
