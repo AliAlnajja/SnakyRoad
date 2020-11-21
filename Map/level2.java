c3fcacd89ff844ddaa01d9920f33dff25a96f460
Ali Alnajjar <Ali.nj.95@hotmail.com>
2020-11-21 01:29:16 -0500

    lvl2 edited

--- Map/level2.java
+++ Map/level2.java
@@ -1,21 +1,51 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class level2 here.
+ * Write a description of class Level2 here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class level2 extends World
+public class Level2 extends World
 {
 
     /**
-     * Constructor for objects of class level2.
+     * Constructor for objects of class Level2.
      * 
      */
-    public level2()
+    int [][] map = 
+           {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+            {0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2},
+            {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2},
+            {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2},
+            {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2},
+            {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2},
+            {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2},
+            {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2},
+            {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+            {0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0},
+            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}};
+    public Level2()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(750, 750, 1); 
+        for(int row = 0; row < 16; row++)
+        for(int col = 0; col < 16; col++)
+            if(map[row][col] == 0)
+                addObject(new MudGrass(), col * 50, row * 50);
+            else if (map[row][col] == 1){
+                addObject(new Dirt(), col * 50, row * 50);
+            }
+            else if (map[row][col] == 2){
+                addObject(new Water1(), col * 50, row * 50);
+            }
+                //addObject(new Road(), 375, 556);
+                //addObject(new Road(), 375, 407);
+                addObject(new Eggs(), 375, 30);
     }
 }
