cefc152e24be07f7e62998da2b171f3e3c25e0d2
Christopher <chrsitopher.andrada14@gmail.com>
2020-11-20 22:36:07 -0500

    Added some player controlled movements for the Snake as well as some animation

---
+++ Map/Snake.java
@@ -0,0 +1,74 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Snake here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Snake extends Actor
+{ private GreenfootImage image1 =  new GreenfootImage("snake1.png");
+    private GreenfootImage image2 =  new GreenfootImage("snake2.png");
+    private GreenfootImage image3 =  new GreenfootImage("snake3.png");
+    private GreenfootImage image4 =  new GreenfootImage("snake4.png");
+    private GreenfootImage image5 =  new GreenfootImage("snake5.png");
+    private GreenfootImage image6 =  new GreenfootImage("snake6.png");
+    private GreenfootImage image7 =  new GreenfootImage("snake7.png");
+    private GreenfootImage image8 =  new GreenfootImage("snake8.png");
+    
+    
+    int frame = 1;
+    int skipFrame = 0;
+    /**
+     * Act - do whatever the Snake wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        checkKeyPressed();
+        skipFrame++;
+    }
+    
+    public void animate(){
+        if (skipFrame % 8 == 0){
+        if (frame==1){
+            setImage(image2);
+        } else if (frame==2){
+            setImage(image3);
+        } else if (frame==3){
+            setImage(image4);
+        } else if (frame==4){
+            setImage(image5);
+        } else if (frame==5){
+            setImage(image6);
+        } else if (frame==6){
+            setImage(image7);
+        } else if (frame==7){
+            setImage(image8);
+        } else if (frame==8){
+            setImage(image1);
+            frame=1;
+        }
+        frame++;
+        }
+    }
+    
+    public void checkKeyPressed()
+    {
+        if (Greenfoot.isKeyDown("up"))
+        {
+            move(3);
+            animate();
+        }
+        if (Greenfoot.isKeyDown("left"))
+        {
+            turn(-5);
+            animate();
+        }
+        if (Greenfoot.isKeyDown("right"))
+        {
+            turn(5);
+            animate();
+        }
+    }  
+}
