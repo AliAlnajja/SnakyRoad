ec03001d3c0c8370f165e93f35752414ad3faaf0
Ali Alnajjar <Ali.nj.95@hotmail.com>
2020-11-21 22:14:43 -0500

    idk

---
+++ Map/Log.java
@@ -0,0 +1,23 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Log here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Log extends Actor
+{
+    /**
+     * Act - do whatever the Log wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Log()
+    {
+        getImage().scale(100,30);
+    }
+    public void act() 
+    {
+        // Add your action code here.
+    }    
+}
