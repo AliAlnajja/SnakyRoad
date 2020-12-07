64d25a6a9ea506a4208dc938dbba6b921cc44f3b
carla oliverio <70611889+CarlaOliverio@users.noreply.github.com>
2020-12-06 23:44:05 -0500

    Press to start , Updated imaged

--- Alpha/Intro.java
+++ Alpha/Intro.java
@@ -18,5 +18,26 @@ public class Intro extends World
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(750, 750, 1); 
         addObject(new PressS(), 375, 375);
+        prepare();
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void act()
+    {
+        if (Greenfoot.isKeyDown("space"))
+            Greenfoot.setWorld(new Level1());
+    }
+
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
