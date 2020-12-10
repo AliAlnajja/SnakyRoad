df06e428eadd6664168c12266fed808edb4bb2fc
Christopher <chrsitopher.andrada14@gmail.com>
2020-12-10 13:06:42 -0500

    Added game over screen

--- Alpha/Snake.java
+++ Alpha/Snake.java
@@ -213,7 +213,7 @@ public class Snake extends Actor
         if(life==0)
         {
             Greenfoot.stop();
-            getWorld().showText("Game Over", 375,375);
+            Greenfoot.setWorld(new GameOver());
         }
         setLocation(373, 709);
     }
