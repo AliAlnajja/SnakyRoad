0b8930df6226131957d84559afa11ee089d3b0d6
Christopher <chrsitopher.andrada14@gmail.com>
2020-12-13 22:02:13 -0500

    Added Images?

--- Alpha/MainMenu.java
+++ Alpha/MainMenu.java
@@ -17,7 +17,7 @@ public class MainMenu extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(800, 500, 1); 
-        prepare();
+        prepare(); 
     }
     /**
      * Prepare the world for the start of the program.
